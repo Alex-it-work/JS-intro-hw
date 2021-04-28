38dd6f2b62de9fb2fa1768eb60feffb554710354
Alex-it-work <Mr.Kirieiev@gmail.com>
2021-04-28 20:48:22 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+/* ********************************* */
+const firstArgument = Math.floor(Math.random() * 100);
+const secondArgument = Math.floor(Math.random() * 100);
+console.log("first = ", firstArgument, ", and second = ", secondArgument);
+console.log("multiplication result is ", firstArgument * secondArgument);
+/* ********************************* */
+const thirdArgument = Math.floor(Math.random() * 100);
+const fourthArgument = Math.floor(Math.random() * 100);
+console.log("third = ", thirdArgument, ", and fourth = ", fourthArgument);
+console.log("division result is ", thirdArgument / fourthArgument);
+/* ********************************* */
+const fifthArgument = Math.floor(Math.random() * 100);
+const sixthArgument = Math.floor(Math.random() * 100);
+console.log("first = ", fifthArgument, ", and second = ", sixthArgument);
+console.log("multiplication result is ", fifthArgument + sixthArgument);
+/* ********************************* */
+/* ********************************* */
+/* ********************************* */
+/* ********************************* */
+/* ********************************* */
+/* ********************************* */
+/* ********************************* */
